7f13542e1ef345589a37857ce81755702971b7e1
prosenjitkumar <prosenjitearnkumar@gmail.com>
2019-01-01 13:18:24 +0600

    todays date added in details page bottom where lie comment seen section has styaed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import BlogPost
+from django.utils import timezone
+
 
 
 class BlogHomeView(ListView):
@@ -13,6 +15,11 @@ class BlogHomeView(ListView):
 class BlogDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/single-post-1.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
 
 
 
