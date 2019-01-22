28cf0bc3575993f47e2829dfcba164249be14bcd
prosenjitkumar <prosenjitearnkumar@gmail.com>
2019-01-22 13:45:47 +0600

    portfolio model added

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,9 +4,6 @@ from django.db import models
 class Gallery(models.Model):
     image = models.ImageField(upload_to='gallery', blank=True)
 
-    def __str__(self):
-        return self.image
-
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
