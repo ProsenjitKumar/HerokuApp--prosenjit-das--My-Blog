a3f0ddd063d95fa99d2e3c9465812b6988cd467c
prosenjitkumar <prosenjitearnkumar@gmail.com>
2019-01-22 11:02:18 +0600

    portfolio model added

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import (
+    Portfolio,
+    Skills,
+    Projects,
+    CurrentPosition,
+    Experience,
+    EducationalBackground,
+)
 
-# Register your models here.
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['name', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(Portfolio, PortfolioAdmin)
+
+
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'percentage', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(Skills, SkillsAdmin)
+
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'website', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(Projects, ProjectsAdmin)
+
+
+class CurrentPositionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'website', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(CurrentPosition, CurrentPositionAdmin)
+
+
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'website', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(Experience, ExperienceAdmin)
+
+
+class EducationalBackgroundAdmin(admin.ModelAdmin):
+    list_display = ['degree', 'subject', 'year', 'result', 'updated']
+    search_fields = ['name']
+
+
+admin.site.register(EducationalBackground, EducationalBackgroundAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=88)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    percentage = models.PositiveSmallIntegerField(verbose_name='percentage')
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Projects(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    website = models.URLField(blank=False)
+    source_code = models.URLField(blank=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    # slug = models.SlugField(prepopulate_from=('name',))
+
+    def __str__(self):
+        return self.name
+
+
+class CurrentPosition(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    website = models.URLField(blank=False)
+    source_code = models.URLField(blank=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+class Experience(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    website = models.URLField(blank=False)
+    source_code = models.URLField(blank=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+class EducationalBackground(models.Model):
+    degree = models.CharField(max_length=30)
+    institute = models.CharField(max_length=255)
+    subject = models.CharField(max_length=30)
+    year = models.CharField(max_length=30)
+    result = models.CharField(max_length=30)
+    board = models.CharField(max_length=30)
+    present_session = models.CharField(max_length=60)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.degree
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=88)
+    headline = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='portfolio/', blank=True)
+    goal = models.TextField()
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
